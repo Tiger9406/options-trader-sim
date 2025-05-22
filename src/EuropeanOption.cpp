12d661901fd608de6450e017fdbb65bf73306886
Tiger <xcao2005@gmail.com>
2025-05-22 10:14:07 -0400

    add EuropeanOption class; declare necessary variables and functions in EuropeanOption.h

---
+++ src/EuropeanOption.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xcao2 on 5/22/2025.
+//
+
+#include "EuropeanOption.h"
---
+++ src/EuropeanOption.h
@@ -0,0 +1,27 @@
+//
+// Created by xcao2 on 5/22/2025.
+//
+
+#ifndef OPTIONS_SIMULATOR_EUROPEANOPTION_H
+#define OPTIONS_SIMULATOR_EUROPEANOPTION_H
+
+//have to clearly distinguish between call & put
+enum class OptionType {CALL, PUT};
+
+class EuropeanOption {
+public:
+    EuropeanOption(double spot_price, double exercise_price, double interest_rate,
+                   double time_expiry, double volatility, OptionType type);
+    double getPrice() const;
+private:
+    double spot_price;
+    double exercise_price;
+    double interest_rate;
+    double time_expiry;
+    double volatility;
+    OptionType type;
+    double calculateBlackScholesPrice() const;
+};
+
+
+#endif //OPTIONS_SIMULATOR_EUROPEANOPTION_H
