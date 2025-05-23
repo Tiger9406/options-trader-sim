b4e2cb5f9551bd39fbdbdd90c672d9fcfa0c9de5
Tiger <xcao2005@gmail.com>
2025-05-23 06:44:30 -0400

    simulate market conditions and create example callOption and putOptions Finalize initial implementation of European Options

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "src/EuropeanOption.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //simulate market conditions
+    double S = 100.0;
+    double K = 101.0;
+    double r = 0.04;
+    double Time = 1.5; //1.5 years
+    double vol = 0.1; // 10% volatility
+
+//    EuropeanOption callOption = EuropeanOption(S, K, r, Time, vol, OptionType::CALL);
+    EuropeanOption callOption(S, K, r, Time, vol, OptionType::CALL);
+    cout<<"European Call Option Price: " << callOption.getPrice()<<endl;
+
+    EuropeanOption putOption(S, K, r, Time, vol, OptionType::PUT);
+    cout<<"European Put Option Price: " <<putOption.getPrice()<<endl;
+
     return 0;
 }
