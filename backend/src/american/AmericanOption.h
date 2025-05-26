99a7240040b1e625f7d2d0bc6e3f38e9b5222927
Tiger <xcao2005@gmail.com>
2025-05-25 21:43:51 -0400

    [feature] add American Options as header; researching binomial trees

---
+++ backend/src/american/AmericanOption.h
@@ -0,0 +1,27 @@
+//
+// Created by xcao2 on 5/25/2025.
+//
+
+#ifndef OPTIONS_SIMULATOR_AMERICANOPTION_H
+#define OPTIONS_SIMULATOR_AMERICANOPTION_H
+
+#include "../shared/OptionType.h"
+
+struct AmericanCallOption {
+    double S, K, T, r, sigma;
+    static constexpr OptionType type = OptionType::Call;
+
+    AmericanCallOption(double S_, double K_, double r_, double sigma_, double T_)
+            : S(S_), K(K_), T(T_), r(r_), sigma(sigma_) {}
+};
+
+struct AmericanPutOption {
+    double S, K, T, r, sigma;
+    static constexpr OptionType type = OptionType::Put;
+
+    AmericanPutOption(double S_, double K_, double r_, double sigma_, double T_)
+            : S(S_), K(K_), T(T_), r(r_), sigma(sigma_) {}
+};
+
+
+#endif //OPTIONS_SIMULATOR_AMERICANOPTION_H
