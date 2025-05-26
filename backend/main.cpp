f5c052c32274dda4070716bce9650548d65be9c0
Tiger <xcao2005@gmail.com>
2025-05-26 09:44:49 -0400

    [testing] implement American Binary Tree pricing & time it in main.cpp; significantly faster than black scholes?

--- backend/main.cpp
+++ backend/main.cpp
@@ -4,6 +4,8 @@
 #include "EuropeanOption.h"
 #include "OptionType.h"
 #include "BlackScholes.h"
+#include "BinomialTree.h"
+#include "AmericanOption.h"
 
 int main() {
     // Simulate market conditions
@@ -13,19 +15,23 @@ int main() {
     double T = 1.5;
     double sigma = 0.1;
 
+    auto start = std::chrono::high_resolution_clock::now();
+    double val;
+    auto end = std::chrono::high_resolution_clock::now();
+
     // ===== INHERITANCE TEST =====
     std::unique_ptr<Option> callOption = std::make_unique<EuropeanOptionInheritance>(S, K, r, sigma, T, Call);
 
-    auto start = std::chrono::high_resolution_clock::now();
-    double val_inheritance = callOption->price();
-    auto end = std::chrono::high_resolution_clock::now();
+    start = std::chrono::high_resolution_clock::now();
+    val = callOption->price();
+    end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::nano> duration_inheritance = end - start;
 
     // ===== TEMPLATE TEST =====
     EuropeanOption callOption_Template(S, K, r, sigma, T, Call);
 
     start = std::chrono::high_resolution_clock::now();
-    double val_template = BlackScholesGeneric<EuropeanOption>::price(callOption_Template);
+    val = BlackScholesGeneric<EuropeanOption>::price(callOption_Template);
     end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::nano> duration_template = end - start;
 
@@ -33,15 +39,24 @@ int main() {
     EuropeanCallOption callOption_Separate(S, K, r, sigma, T);
 
     start = std::chrono::high_resolution_clock::now();
-    double val_template_separate = BlackScholes<EuropeanCallOption>::price(callOption_Separate);
+    val = BlackScholes<EuropeanCallOption>::price(callOption_Separate);
     end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::nano> duration_template_separate = end - start;
+
+    // ==== TEMPLATE SEPARATE AMERICAN PUT TEST ====
+    AmericanPutOption putOption_Separate(S, K, r, sigma, T);
+
+    start = std::chrono::high_resolution_clock::now();
+    val = BinomialTree<AmericanPutOption>::price(putOption_Separate);
+    end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::nano> american_duration = end - start;
 
 
     // Output results
     std::cout << "Inheritance price():       " << duration_inheritance.count() << " ns\n";
     std::cout << "Template price():          " << duration_template.count() << " ns\n";
     std::cout << "Template Separate price(): " << duration_template_separate.count() << " ns\n";
+    std::cout << "American BT price():       " << american_duration.count() << " ns\n";
 
     return 0;
 }
