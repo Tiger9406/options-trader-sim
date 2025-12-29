5a5304e71b5c275fc2fb37c5f1da177e2850ac9f
Tiger <xcao2005@gmail.com>
2025-12-29 10:45:35 -0500

    change executable to be main.cpp; find packages openssl. main establishes connection to deribit

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -6,14 +6,29 @@
 #include "api/ApiKeyLoader.h"
 #include "api/Client.h"
 
+#include "api/DataManager.h"
+
 int main(){
-    //use polygon api
-    constexpr const char* host = "api.polygon.io";
-    constexpr const char* port = "80";
-    const std::string api_key = load_api_key();
-    const std::string target = "/v3/snapshot/options/TSLA?apiKey=" + api_key;
-    constexpr int version = 11; //http/1.1
+    // constexpr const char* host = "api.polygon.io";
+    // constexpr const char* port = "80";
+    // const std::string api_key = load_api_key();
+    // const std::string target = "/v3/snapshot/options/TSLA?apiKey=" + api_key;
+    // constexpr int version = 11; //http/1.1
 
+    auto ioc = std::make_shared<net::io_context>();
+    auto ctx = std::make_shared<ssl::context>(ssl::context::tlsv12_client);
+    
+    // data manager; shared pointer for client
+    auto data_manager = std::make_shared<DataManager>();
 
-    //use multithreading queue as inspired by hrt: multithreading in toucanserver
+    // create client to connect to deribit
+    auto client = std::make_shared<Client>(*ioc, *ctx);
+    
+    // capture data_manager by value (increasing ref count)
+    client->set_callback([data_manager](std::string msg) {
+        data_manager->update_from_json(std::move(msg));
+    });
+
+    client->run("www.deribit.com", "443", "BTC-2JAN26-90000-C");
+    ioc->run();
 }
